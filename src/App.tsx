a6875e58c4558ee85be4819437456aea96536110
VictorJGB <victorgb.dev@gmail.com>
2025-09-22 17:23:31 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div className='bg-primary size-full items-center justify-center'>
+      <h1>Guardia Maria API</h1>
+    </div>
+  )
+}
